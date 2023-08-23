eb28b205157938037bac81e7125b6a3815dc4796
robertoignacio <rvs.git@gmail.com>
2023-08-23 02:45:33 -0400

    Inform the user about the status of the login flow

--- app/components/SendLinkForm.tsx
+++ app/components/SendLinkForm.tsx
@@ -1,51 +1,91 @@
+"use client";
+
 import React, { useState, ChangeEvent } from 'react';
 import { auth } from "../../firebaseConfig";
 import { sendSignInLinkToEmail } from "firebase/auth";
 
 // instructions for firebase on how to construct the email link
 const actionCodeSettings = {
-    url: "http://localhost:3000/panel", // the URL to redirect to after a successful login
-    handleCodeInApp: true, // whether to open the link in a mobile app or a web page in a mobile browser
+    url: "http://localhost:3000/panel", // the URL to redirect to after clicking the link in the email
+    handleCodeInApp: true, // open the link on the web app page (even if it was clicked on a mobile device)
 };
 
-
+// ---------------------------------------------------------
 // the React Component
 export default function SendLinkForm() {
-    type User = {
-        email: string;
-    }
-
+    type User = { email: string; }
     const [email, setEmail] = React.useState<User["email"]>("");
-
+  
     // update the state of the email variable by dispatching an event when the value of the HTML input email changes
     const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         // update the state (a React object)
         setEmail(event.target.value)
-        // passes the value to the email variable outside the function
-
+        // passes the value to the email variable outside the handleEmailChange function
     }
+    
 
-    // handles the submit event at the form submit button
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-        // prevent the default behavior of the submit event
+    // ---------------------------------------------------------
+    // what to do with the submit event at the form submit button.
+    const handleEmailSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        // prevent a page reload, which is the default behavior of the submit event
         event.preventDefault();
 
-        // send the link to the user’s email
-        sendSignInLinkToEmail(auth, email, actionCodeSettings).then(() => {
-            // The link was successfully sent. Inform the user.
+        /* 
+         * basic sign-in flow:
+         * 1. The SuperAdmin must first create the User at the Identity console. Known and verified email addresses.
+         * (User creation by an Admin at the webapp level will be coded later)
+         * 2. User actions of account creation and deletion are disabled at the server level for all Users.
+         * 3. The visitor writes the email at the form html input email field.
+         * 4. The visitor clicks the submit button.
+         * 5. If the email exists at the User collection, then the server sends an email to the email address.
+         * 6. If the email does not exist at the User collection, then the server raises an error HTTP 400 Bad Request.
+         */
+
+        // Inform the user about the status of the login flow
+        const loginFlowStatus = document.getElementById("login-flow-status");
+
+        // Attempts to send the email to the user
+        sendSignInLinkToEmail(auth, email, actionCodeSettings)
+          .then((response) => {
+            // Promise: Will send the email link when the HTTP query returns that the email address submitted exists at the User collection.
+            // The link was successfully sent. 
+            if(response === undefined) {
+              // will be undefined as a security measure
+              loginFlowStatus!.textContent = "Se envió un enlace a su dirección de email. Revise su casilla de correo.";
+              // the email template is terrible, but to get a custom template I will have to setup a STMP server
+
+              // disable HTML input email field
+              const emailInput = document.getElementById("email-input");
+              emailInput!.setAttribute("disabled", "true");
+
+              // disable HtmlElement button (should also block the submit event)
+              const submitButton = document.getElementById("submit-button");
+              submitButton!.setAttribute("disabled", "true");
+              
+              // clear the email input field
+              setEmail("");
+
+              // change the text of the submit button
+              submitButton!.textContent = "Enlace enviado";
+
+              // change style of the submit button
+              submitButton!.classList.remove("bg-teal-800");
+              submitButton!.classList.add("bg-teal-400");
+            }
 
             // Save the email locally so you don't need to ask the user for it again
-            // if they open the link on the same device.
+            // when they open the link on the same device.
             window.localStorage.setItem('emailForSignIn', email);
+            // window is a client object, not available at the server
+          })
+          .catch((error) => {
+            // console.log(JSON.stringify(error)
 
-        })
-        .catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            // handle errors here
-        });
-
-        // do not pass the user’s email in the redirect URL parameters!
+            // Inform the user about the status of the login flow: error
+            if(error.code) {
+              loginFlowStatus!.textContent = "No se pudo enviar el enlace. La dirección de email no es la correcta.";
+            }
+          });
     }
 
 
@@ -53,32 +93,31 @@ export default function SendLinkForm() {
         <div className="w-1/2 mx-auto">
         <h2 className="mt-4 text-center font-bold text-lg">Consultar documentos de avance</h2>
   
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleEmailSubmit}>
           <div className="my-2 flex flex-col">
             <div className="text-xs">
-                <p>Escriba la dirección de e-mail correspondiente a su cuenta de Socio.</p>
-                <p>No necesita contraseña.</p>
-                <p>Se le enviará un enlace a su dirección de email, para ingresar</p>
+                <p>Escriba la dirección de e-mail ya verificada, correspondiente a su cuenta de Socio. No necesita contraseña.</p>
+                <p>Se le enviará un enlace a su dirección de email para ingresar</p>
             </div>
  
             <label className="mt-4 text-teal-800 font-serif">Dirección de e-mail</label>
             <input 
+              id="email-input" 
               onChange={handleEmailChange} 
               value={email}
               type="email" 
-              placeholder="socio@email.com"
+              placeholder="nombre@dominio.com"
               className="my-2 bg-none border-2 border-teal-900 p-1 rounded-xl font-bold text-teal-800" 
               required
             />
   
             <button 
+              id="submit-button" 
               type="submit"
               className="bg-teal-800 text-white font-bold p-2 rounded-xl mt-4">Enviar link de acceso</button>
             
             <div>
-                <p>Update stage on the flow process</p>
-                <p className="text-xs mt-4">Si no recibe el link de acceso, revise su carpeta de spam.</p>
-                <p className="text-xs">Si el problema persiste, envíe un email a </p>
+                <p id="login-flow-status" className="text-xs"></p>
             </div>
   
             
--- app/panel/page.tsx
+++ app/panel/page.tsx
@@ -1,40 +1,52 @@
 "use client";
-// complete the sign in on landing page
-import { isSignInWithEmailLink, signInWithEmailLink } from "firebase/auth";
-import { auth } from "../../firebaseConfig";
-import { signOut } from "firebase/auth";
 
+import { isSignInWithEmailLink, signInWithEmailLink, signOut } from "firebase/auth";
+import { auth } from "../../firebaseConfig";
+import { useEffect } from 'react';
+
+
+// ---------------------------------------------------------
 // the React Component
 export default function DashboardPage() {
 
-    // At the landing page, confirm the incoming link is a sign-in with email link.
-
-    if (isSignInWithEmailLink(auth, window.location.href)) {
-        // get the email if available
-    let email = window.localStorage.getItem('emailForSignIn');
-    // window es un objeto de cliente que representa la ventana del navegador
-
-    if (!email) {
-        // User opened the link on a different device. 
-        // To prevent session fixation attacks, ask the user to provide the associated email again. 
-        // For example:
-        email = window.prompt('Please provide your email for confirmation');
-    }
-    // The client SDK will parse the code from the link for you.
-
-    // note the ! after email, which tells TypeScript that email will not be null or undefined
-    signInWithEmailLink(auth, email!, window.location.href)
-        .then((result) => {
-        // Clear email from storage.
-        window.localStorage.removeItem('emailForSignIn');
-        // You can access the new user via result.user
-
-        })
-        .catch((error) => {
-        // Some error occurred, you can inspect the code: error.code
-        // Common errors could be invalid email and invalid or expired OTPs.
-        });
-    }
+    // Because error ReferenceError: window is not defined
+    useEffect(() => {
+        // complete the sign-in on the landing page
+        // At the landing page, confirm the incoming link is a sign-in with email link.
+        if (isSignInWithEmailLink(auth, window.location.href)) {
+            // get the email if available
+            let email = window.localStorage.getItem('emailForSignIn');
+            // window is a client object, not available at the server
+    
+            // If there is no email saved at the local storage
+            // instead of email check:
+            // build a boolean function to detect if the user is logged in. 
+            // TBA
+    
+            if (!email) {
+                // send the user back to the home page, TBA router nesting to protect the route
+                window.location.href = "/";  
+            }            
+    
+            // Promise to TypeScript that email value will never be null or undefined.
+            signInWithEmailLink(auth, email!, window.location.href)
+                .then((result) => {
+                // Clear email from storage, without it becoming null or undefined
+                window.localStorage.setItem('emailForSignIn', "");
+                // You can access the new user via result.user
+                // Additional user info profile not available via:
+                // result.additionalUserInfo.profile == null
+                // You can check if the user is new or existing:
+                // result.additionalUserInfo.isNewUser
+                console.log("Sign-in successful");
+                console.log(result);
+                })
+                .catch((error) => {
+                    console.log(error.code);
+            });
+        }
+    }, []); // empty array to run only once
+    
 
     const handleLogOut = () => {
         signOut(auth).then(() => {
@@ -52,7 +64,7 @@ export default function DashboardPage() {
             <h1 className="mt-4 text-center font-bold text-lg">Documentos publicados</h1>
             <p className="text-xs">En esta página se publicarán los documentos de avance del proyecto</p>
             <button 
-                onSubmit={handleLogOut}
+                onClick={handleLogOut}
                 type="submit" 
                 className="bg-teal-800 text-white font-bold p-2 rounded-xl mt-4">
                     Cerrar sesión
